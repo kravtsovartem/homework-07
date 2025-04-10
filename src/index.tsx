cd27fc6fc7a91931795341bdc3860c180a9757cd
Артём Кравцов <43048838+kravtsovartem@users.noreply.github.com>
2025-04-11 00:41:04 +0300

    feat: Добавлен стор, хуки и провайдер стора

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import {MainApp} from './apps/MainApp';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { MainApp } from "./apps/MainApp";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "src/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MainApp />
+    <Provider store={store}>
+      <MainApp />
+    </Provider>
   </React.StrictMode>
 );
 
