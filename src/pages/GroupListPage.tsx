6d5e6051a27530fe46a6f99831830a3d4e2fce1d
Артём Кравцов <43048838+kravtsovartem@users.noreply.github.com>
2025-04-11 00:51:17 +0300

    fix(GroupListPage.tsx): Убраны пропсы, данные берутся из стора

--- src/pages/GroupListPage.tsx
+++ src/pages/GroupListPage.tsx
@@ -1,12 +1,14 @@
-import React, {memo} from 'react';
-import {CommonPageProps} from './types';
-import {Col, Row} from 'react-bootstrap';
-import {GroupContactsCard} from 'src/components/GroupContactsCard';
+import { memo } from "react";
+import { Col, Row } from "react-bootstrap";
+import { GroupContactsCard } from "src/components/GroupContactsCard";
+import { useAppSelector } from "src/store/hooks";
 
-export const GroupListPage = memo<CommonPageProps>(({contactsState, groupContactsState}) => {
+export const GroupListPage = memo(() => {
+  const { listGroupContacts } = useAppSelector((state) => state.contacts);
+
   return (
     <Row xxl={4}>
-      {groupContactsState[0].map((groupContacts) => (
+      {listGroupContacts.map((groupContacts) => (
         <Col key={groupContacts.id}>
           <GroupContactsCard groupContacts={groupContacts} withLink />
         </Col>
