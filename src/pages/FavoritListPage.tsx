7d148fff4abd1a861db8b689b6baee82a79bc6d6
Артём Кравцов <a.kravtsov@globexit.ru>
2025-04-24 00:55:59 +0300

    feat: Изменены страницы вывода избранных контактов

--- src/pages/FavoritListPage.tsx
+++ src/pages/FavoritListPage.tsx
@@ -1,33 +1,32 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { ContactCard } from "src/components/ContactCard";
 import { ContactDto } from "src/types/dto/ContactDto";
 import { useAppSelector } from "src/store/hooks";
+import { useGetContactsQuery } from "src/store/contacts";
 
-export const FavoritListPage = memo(
-  () => {
-		
-		const { listContacts, listFavoriteContacts } = useAppSelector(
-			(state) => state.contacts
-		);
+export const FavoritListPage = memo(() => {
+  const listFavoriteContacts = useAppSelector(
+    (state) => state.favoriteContacts
+  );
 
-    const [contacts, setContacts] = useState<ContactDto[]>([]);
-		
-    useEffect(() => {
-      setContacts(() =>
-        listContacts.filter(({ id }) =>
-          listFavoriteContacts.includes(id)
-        )
-      );
-    }, [listContacts, listFavoriteContacts]);
-    return (
-      <Row xxl={4} className="g-4">
-        {contacts.map((contact) => (
-          <Col key={contact.id}>
-            <ContactCard contact={contact} withLink />
-          </Col>
-        ))}
-      </Row>
+  const { data: contactsData } = useGetContactsQuery();
+  const listContacts = useMemo(() => contactsData ?? [], [contactsData]);
+
+  const [contacts, setContacts] = useState<ContactDto[]>([]);
+
+  useEffect(() => {
+    setContacts(() =>
+      listContacts.filter(({ id }) => listFavoriteContacts.includes(id))
     );
-  }
-);
+  }, [listContacts, listFavoriteContacts]);
+  return (
+    <Row xxl={4} className="g-4">
+      {contacts.map((contact) => (
+        <Col key={contact.id}>
+          <ContactCard contact={contact} withLink />
+        </Col>
+      ))}
+    </Row>
+  );
+});
