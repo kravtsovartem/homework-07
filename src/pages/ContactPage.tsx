9d8c5179f52ce9f3af486a5f32da80647af07abf
Артём Кравцов <a.kravtsov@globexit.ru>
2025-04-24 11:32:16 +0300

    refactor: Удалены неиспользуемые файлы и зависимости

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { ContactDto } from "src/types/dto/ContactDto";
 import { ContactCard } from "src/components/ContactCard";
 import { Empty } from "src/components/Empty";
 import { useGetContactsQuery } from "src/store/contacts";
--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,8 +1,5 @@
-import { TypedUseSelectorHook, useDispatch, useSelector, useStore } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
-import { ActionsType } from "./actions";
+import { TypedUseSelectorHook, useSelector, useStore } from "react-redux";
 import { RootState } from "./";
 
-export const useAppDispatch = useDispatch<ThunkDispatch<RootState, void, ActionsType>>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore = useStore<RootState>
