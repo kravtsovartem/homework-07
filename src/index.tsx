1f7f6e6793e4d078ec8f7d2b6a2895e29c24d20f
Артём Кравцов <a.kravtsov@globexit.ru>
2025-04-30 01:25:07 +0300

    feat: Убран Provider от Redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { MainApp } from "./apps/MainApp";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "src/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <MainApp />
-    </Provider>
   </React.StrictMode>
 );
 
